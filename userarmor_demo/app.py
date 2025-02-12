b8c1db96dabea0d0bbebc026270aa4a87806567a
malvi <mario.alviano@gmail.com>
2025-02-12 21:25:29 +0100

    move to poetry setup unconfined app

--- userarmor_demo/app.py
+++ userarmor_demo/app.py
@@ -1,20 +1,12 @@
 from flask import Flask, render_template, request
-import clingo
+import subprocess
 
 app = Flask(__name__)
 
+
 def run_clingo(program):
-    output = []
+    return subprocess.run(["clingo"], input=program, text=True, capture_output=True).stdout
 
-    def on_model(model):
-        output.append(f"Soluzione: {', '.join(str(atom) for atom in model.symbols(atoms=True))}")
-
-    ctl = clingo.Control()
-    ctl.add("base", [], program)
-    ctl.ground([("base", [])])
-    ctl.solve(on_model=on_model)
-
-    return "\n".join(output) if output else "Nessuna soluzione trovata."
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -27,6 +19,7 @@ def home():
             output = f"Errore nell'esecuzione di Clingo: {str(e)}"
 
     return render_template('index.html', output=output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
