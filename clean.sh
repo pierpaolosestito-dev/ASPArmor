47b9742fe8fb14a8db7fd922aa0d4c1c401f65de
malvi <mario.alviano@gmail.com>
2025-02-12 23:49:22 +0100

    fixes here and there add userarmor (to be finalized with the actual commands)

--- clean.sh
+++ clean.sh
@@ -1,3 +1,10 @@
 sudo rm -rf /var/www/clingo_input
+
+sudo aa-complain /etc/apparmor.d/usr.bin.clingo
+sudo apparmor_parser -R /etc/apparmor.d/usr.bin.clingo
+
 sudo rm -f /etc/apparmor.d/usr.bin.clingo
 sudo rm -rf /etc/apparmor.d/.usr.bin.clingo
+
+sudo systemctl restart apparmor
+
--- run-confined-via-userarmor.sh
+++ run-confined-via-userarmor.sh
@@ -1,14 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+
+ROOT_PATH=`pwd`
 
 source clean.sh
 
-# METTI QUI I COMANDI PER IMPOSTARE USERARMOR
-# COPIA DEI PROFILI, COMANDI USATI e via dicendo
 sudo apt install apparmor-utils
+
 sudo cp profile_userarmor /etc/apparmor.d/usr.bin.clingo
-sudo userarmor process usr.bin.clingo
+sudo cp -r .usr.bin.clingo /etc/apparmor.d/
+
+sudo mv /etc/apparmor.d/.usr.bin.clingo/USER /etc/apparmor.d/.usr.bin.clingo/$USER
+
+sudo sed -i "s/USER/$USER/g" /etc/apparmor.d/usr.bin.clingo
+sudo sed -i "s/USER/$USER/g" /etc/apparmor.d/.usr.bin.clingo/$USER
+sudo sed -i "s/USER/$USER/g" /etc/apparmor.d/.usr.bin.clingo/mappings
+
+# sudo userarmor process usr.bin.clingo
+sudo aa-enforce /etc/apparmor.d/usr.bin.clingo  # da eliminare (deve farlo il comando userarmor)
+
 
 source setup_env.sh
 
-CLINGO_PREFIX="usercli launch" poetry run python app.py
+#CLINGO_PREFIX="$ROOT_PATH/usercli launch" poetry run python app.py
+CLINGO_PREFIX="sudo aa-exec -p /usr/bin/clingo//$USER " poetry run python app.py   # da eliminare (deve farlo con il prefisso di sopra)
 
