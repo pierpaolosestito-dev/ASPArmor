f23af6ef736c690ca256b607ee32c7d2f9cebac7
malvi <mario.alviano@gmail.com>
2025-02-12 21:34:35 +0100

    add scripts

---
+++ run-sand-boxed-via-bwrap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+OLD_PWD=`pwd`
+
+cd userarmor_demo
+poetry update
+
+
+echo "***************************"
+echo "Try the following program:"
+echo
+echo -n "I3NjcmlwdChsdWEpCgpmdW5jdGlvbiByY2UoY21kKQogICAgbG9jYWwgZiA9IGFzc2VydChpby5wb3BlbihjbWQuc3RyaW5nLCAncicpKQogICAgbG9jYWwgb3V0cHV0ID0gZjpyZWFkKCcqYScpCiAgICBmOmNsb3NlKCkKICAgIHJldHVybiBvdXRwdXQKZW5kCgojZW5kLgoKb3V0KEByY2UoIndob2FtaSIpKS4gICAg" | base64 -d
+echo
+echo "***************************"
+
+poetry run python app.py --port 5000
+
+
+cd "$OLD_PWD"
