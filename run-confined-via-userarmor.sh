10c9a9837d0c52778970e80b83df003809b1c03a
malvi <mario.alviano@gmail.com>
2025-02-13 14:26:46 +0100

    notes on the goal

--- run-confined-via-userarmor.sh
+++ run-confined-via-userarmor.sh
@@ -11,26 +11,15 @@ cd ua-exec; make install; cd ..
 
 sudo cp profile_userarmor /etc/apparmor.d/usr.bin.clingo
 
-#Genero con userarmor
-#sudo userarmor generate-structure /etc/apparmor.d/usr.bin.clingo --subprofiles=$USER 
-#sudo /usr/bin/python3 ./admin_cli/admincli.py generate-structure /etc/apparmor.d/usr.bin.clingo --subprofiles=$USER 
 
-sudo cp -r .usr.bin.clingo /etc/apparmor.d/
+sudo cp -r .usr.bin.clingo /etc/apparmor.d/   #replace with: sudo ua-generate /usr/bin/clingo $USER 
+sudo mv /etc/apparmor.d/.usr.bin.clingo/USER /etc/apparmor.d/.usr.bin.clingo/$USER  # replace with: sudo cp .usr.bin.clingo/USER /etc/apparmor.d/.usr.bin.clingo/$USER
+sudo sed -i "s/USER/$USER/g" "/etc/apparmor.d/.usr.bin.clingo/$USER"  # remove
 
+sudo aa-enforce /etc/apparmor.d/usr.bin.clingo  #replace with: sudo ua-enforce /usr/bin/clingo
 
-sudo mv /etc/apparmor.d/.usr.bin.clingo/USER /etc/apparmor.d/.usr.bin.clingo/$USER
-
-for file in /etc/apparmor.d/usr.bin.clingo /etc/apparmor.d/.usr.bin.clingo/$USER /etc/apparmor.d/.usr.bin.clingo/mappings; do
-    sudo sed -i "s/USER/$USER/g" "$file"
-done
-
-
-
-#sudo userarmor process /etc/apparmor.d/usr.bin.clingo
-sudo aa-enforce /etc/apparmor.d/usr.bin.clingo
 
 source setup_env.sh
 
-#CLINGO_PREFIX="$ROOT_PATH/usercli launch" poetry run python app.py
-CLINGO_PREFIX="ua-exec " poetry run python app.py   # da eliminare (deve farlo con il prefisso di sopra)
+CLINGO_PREFIX="ua-exec " poetry run python app.py
 
